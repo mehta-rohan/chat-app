b5fd7314279a06bc2c83f8d0e68897f260f07332
unknown <rohanmehta0077@gmail.com>
2017-07-29 02:18:18 +0530

    socket added

--- server/server.js
+++ server/server.js
@@ -2,13 +2,25 @@ const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname + './../public');
 const port = process.env.PORT || '3000';
-
-
+const socketIO = require('socket.io');
+const http = require('http');
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+	console.log('Hey I got you');
+
+	socket.on('disconnect',()=>{
+		console.log('User Gone');		
+	})
+});
+
+server.listen(port,()=>{
 	console.log(`sever up @ ${port}`);
 });
 
